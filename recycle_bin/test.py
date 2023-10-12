6195227a60a790869d48e84d6ed561b1f91b60d5
Cristian Pereira <cpereira1042@gmail.com>
2023-10-12 03:11:13 -0500

    test file deleted

--- recycle_bin/test.py
+++
@@ -1,9 +0,0 @@
-from recycle_bin.recycle_one.models import Material
-
-def materials_filter():
-    #crea una lista con los materiales aptos para reciclaje
-    selectable_materials = Material.objects.all()
-    print (selectable_materials)
-
-
-materials_filter()
